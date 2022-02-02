01a00abd589848cad4fa5dcd7346d896340f078a
cmanish049 <cmanish049@gmail.com>
2022-02-02 22:51:25 +0545

    show api

--- app/Http/Controllers/Api/V1/Posts/ShowController.php
+++ app/Http/Controllers/Api/V1/Posts/ShowController.php
@@ -6,12 +6,24 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\Posts\PostResource;
 use Domain\Blogging\Models\Post;
 use Illuminate\Http\Request;
+use Spatie\QueryBuilder\QueryBuilder;
+use Symfony\Component\HttpFoundation\Response;
 
 class ShowController extends Controller
 {
 
     public function __invoke(Request $request, Post $post)
     {
-        return $post;
+        $post = QueryBuilder::for(
+                subject: $post
+            )
+            ->allowedIncludes(
+                includes: ['user']
+            )->first();
+
+        return response()->json(
+            data: new PostResource($post),
+            status: Response::HTTP_OK
+        );
     }
 }
